03c41704ba808fdf065458dc6f403dd9723dcb60
davidSantosFranca <david1995sf@gmail.com>
2024-07-19 15:21:19 -0300

    changed r2 function

--- villermaux.c
+++ villermaux.c
@@ -8,26 +8,26 @@ DEFINE_VR_RATE(vol_reac_rate, c, t, r, mw, yi, rate, rr_t)
     int N = 8;
     // mixture species I, IO3, H, I3, H2BO3, H2O, H3BO3, I2
     real mfI = yi[0];
-    real mlI = mw[0];
+    real mwI = mw[0];
     real mfIO3 = yi[1];
-    real mlIO3 = mw[1];
+    real mwIO3 = mw[1];
     real mfH = yi[2];
-    real mlH = mw[2];
+    real mwH = mw[2];
     real mfI3 = yi[3];
-    real mlI3 = mw[3];
+    real mwI3 = mw[3];
     real mfH2BO3 = yi[4];
-    real mlH2BO3 = mw[4];
+    real mwH2BO3 = mw[4];
     real mfH3BO3 = yi[5];
-    real mlH3BO3 = mw[5];
+    real mwH3BO3 = mw[5];
     real mfI2 = yi[6];
-    real mlI2 = mw[6];
+    real mwI2 = mw[6];
     real mfH2O = yi[7];
-    real mlH2O = mw[7];
+    real mwH2O = mw[7];
 
     if (!strcmp(r->name, "reaction-1"))
     {
         /* Reaction 1 - k1*[H+]*[H2BO3-]*/
-        *rate = k1 * mfH / mlH * mfH2BO3 / mlH2BO3;
+        *rate = k1 * mfH / mwH * mfH2BO3 / mwH2BO3;
     }
     else if (!strcmp(r->name, "reaction-2"))
     {
@@ -35,12 +35,16 @@ DEFINE_VR_RATE(vol_reac_rate, c, t, r, mw, yi, rate, rr_t)
         real molar_fraction[N];
         calculate_molar_fraction(c, t, molar_fraction, mw, N);
         real k2 = calculate_K2(molar_fraction, chargeNumber, N);
-        *rate = k2 * mfH / mlH * mfH / mlH * mfI / mlI * mfI / mlI * mfIO3 / mlIO3;
+        real mcH = mmfH / mwH;
+        real mcI2 = mmfI2 / mwI2;
+        real mcIO3 = mmfIO3 / mwIO3;
+
+        *rate = k2 * pow(mcH, 2) * pow(mcI2, 2) * mcIO3;
     }
     else if (!strcmp(r->name, "reaction-3"))
     {
         /* Reaction 3 - k3f*[I3-] - k3b*[I2]*[I-] */
-        *rate = k3f * mfI3 / mlI3 - k3b * mfI2 / mlI2 * mfI / mlI;
+        *rate = k3f * mfI3 / mwI3 - k3b * mfI2 / mwI2 * mfI / mwI;
     }
     else
     {
