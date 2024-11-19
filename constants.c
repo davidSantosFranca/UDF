5d418a67692aa4cbf3b2e50fd52f522f3e05c9fe
davidSantosFranca <david1995sf@gmail.com>
2024-11-19 04:13:43 -0800

    constants change

--- constants.c
+++ constants.c
@@ -3,4 +3,5 @@
 real k1 = 1E11;   // m^3.kmol^-1.s^-1 
 real k3f = 5.9E9; // m^3.kmol^-1.s^-1
 real k3b = 7.5E6; // s^-1
+// mixture species I, IO3, H, I3, H2BO3, H3BO3, I2, H2O
 real chargeNumber[] = {1, 1, 1, 1, 1, 0, 0, 0};
